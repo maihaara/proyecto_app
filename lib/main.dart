4d8c1e713814ede7fb2d69cda4b204372f02036f
maihaara <maiharodriguezamarilla13@gmail.com>
2024-07-09 14:58:09 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const SpacedItemsList());
+
+class SpacedItemsList extends StatelessWidget {
+  const SpacedItemsList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const items = 4;
+    var datalist =  [{
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "fugiat veniam minus",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "et porro tempora",
+    "completed": true
+  }  ];
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 240, 240, 240)),
+        cardTheme: CardTheme(color: Color.fromARGB(255, 170, 166, 209)),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        body: LayoutBuilder(builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constraints.maxHeight),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: List.generate(
+                    datalist.length, (index) => ItemWidget(text: "${datalist[index]["completed"]}")),
+              ),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
+
+
+class ItemWidget extends StatelessWidget {
+  const ItemWidget({
+    super.key,
+    required this.text,
+  });
+
+  final String text;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: SizedBox(
+        height: 100,
+        child: Center(child: Text(text)),
+      ),
+    );
+  }
+}
